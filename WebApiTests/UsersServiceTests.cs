987582c17befa5264f9f6d15657357e1eb6063f5
ovidiu todea <ovi.todea@yahoo.com>
2019-06-03 21:21:28 +0300

    more User Service tests added

--- WebApiTests/UsersServiceTests.cs
+++ WebApiTests/UsersServiceTests.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using WebApplication3.Models;
 using WebApplication3.Services;
-
+using WebApplication3.ViewModels;
 
 namespace Tests
 {
@@ -45,6 +47,184 @@ namespace Tests
                 Assert.IsNotNull(result);
                 Assert.AreEqual(added.Username, result.Username);
             }
+        }
+
+        [Test]
+        public void InvalidRegisterShouldNotCerateANewUserWithTheSameUsername()
+        {
+            var options = new DbContextOptionsBuilder<MoviesDbContext>()
+                .UseInMemoryDatabase(databaseName: nameof(InvalidRegisterShouldNotCerateANewUserWithTheSameUsername))
+                .Options;
+
+            using (var context = new MoviesDbContext(options))
+            {
+                var userService = new UsersService(context, config);
+
+                var added1 = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "ovi@yahoo.com",
+                    FirstName = "ovi1",
+                    LastName = "todea1",
+                    Password = "123456",
+                    Username = "user_ovi"
+
+                };
+                var added2 = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "todea@yahoo.com",
+                    FirstName = "ovi2",
+                    LastName = "todea2",
+                    Password = "123456",
+                    Username = "user_ovi"
+
+                };
+
+                userService.Register(added1);
+                var result = userService.Register(added2);
+
+                Assert.AreEqual(null, result);
+            }
+
+        }
+        [Test]
+        public void ValidAuthentificationShouldAuthenticateValidUser()
+        {
+            var options = new DbContextOptionsBuilder<MoviesDbContext>()
+                .UseInMemoryDatabase(databaseName: nameof(ValidAuthentificationShouldAuthenticateValidUser))
+                .Options;
+
+            using (var context = new MoviesDbContext(options))
+            {
+                var userService = new UsersService(context, config);
+
+                var addedUser = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "ovi@yahoo.com",
+                    FirstName = "ovi1",
+                    LastName = "todea1",
+                    Password = "123456",
+                    Username = "user_ovi"
+
+                };
+
+                var addResult = userService.Register(addedUser);
+
+                Assert.IsNotNull(addResult);
+                Assert.AreEqual(addedUser.Username, addResult.Username);
+
+                var authentificate = new WebApplication3.ViewModels.UserGetModel
+                {
+                    Email = "ovi@yahoo.com",
+                    Username = "user_ovi"
+                };
+
+                var result = userService.Authenticate(addedUser.Username, addedUser.Password);
+
+                Assert.IsNotNull(result);
+
+                Assert.AreEqual(authentificate.Username, result.Username);
+            }
+
+        }
+        [Test]
+        public void InvalidAuthentificationShouldNotAuthenticateUserWithInvalidPassword()
+        {
+            var options = new DbContextOptionsBuilder<MoviesDbContext>()
+                .UseInMemoryDatabase(databaseName: nameof(InvalidAuthentificationShouldNotAuthenticateUserWithInvalidPassword))
+                .Options;
+
+            using (var context = new MoviesDbContext(options))
+            {
+                var userService = new UsersService(context, config);
+
+                var addedUser = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "ovi@yahoo.com",
+                    FirstName = "ovi1",
+                    LastName = "todea1",
+                    Password = "123456",
+                    Username = "user_ovi"
+
+                };
+
+                var addResult = userService.Register(addedUser);
+
+                Assert.IsNotNull(addResult);
+                Assert.AreEqual(addedUser.Username, addResult.Username);
+
+                var authentificate = new WebApplication3.ViewModels.UserGetModel
+                {
+                    Email = "ovi@yahoo.com",
+                    Username = "user_ovi"
+                };
+
+                var result = userService.Authenticate(addedUser.Username, "012345");
+
+                Assert.AreEqual(null, result);
+            }
+
+        }
+        [Test]
+        public void ValidGetAllShouldReturnAllUsers()
+        {
+            var options = new DbContextOptionsBuilder<MoviesDbContext>()
+                .UseInMemoryDatabase(databaseName: nameof(ValidGetAllShouldReturnAllUsers))
+                .Options;
+
+            using (var context = new MoviesDbContext(options))
+            {
+                var userService = new UsersService(context, config);
+
+                var addedUser1 = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "ovi@yahoo.com",
+                    FirstName = "ovi1",
+                    LastName = "todea1",
+                    Password = "123456",
+                    Username = "user_ovi_1"
+
+                };
+                var addedUser2 = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "ovi@yahoo.com",
+                    FirstName = "ovi1",
+                    LastName = "todea1",
+                    Password = "123456",
+                    Username = "user_ovi_2"
+
+                };
+
+                var addedUser3 = new WebApplication3.ViewModels.RegisterPostModel
+                {
+                    Email = "ovi@yahoo.com",
+                    FirstName = "ovi1",
+                    LastName = "todea1",
+                    Password = "123456",
+                    Username = "user_ovi_3"
+
+                };
+
+                UserGetModel user1 = userService.Register(addedUser1);
+                UserGetModel user2 = userService.Register(addedUser2);
+                UserGetModel user3 = userService.Register(addedUser3);
+
+                List<UserGetModel> actual = new List<UserGetModel>();
+
+                user1.Token = null;
+                user2.Token = null;
+                user3.Token = null;
+
+                actual.Add(user1);
+                actual.Add(user2);
+                actual.Add(user3);
+
+                IEnumerable<UserGetModel> result = userService.GetAll();
+                IEnumerable<UserGetModel> expected = actual.AsEnumerable();
+
+                Assert.IsTrue(expected.SequenceEqual(actual));
+
+            }
+
         }
     }
 }
